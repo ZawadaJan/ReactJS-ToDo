9b33b5ace52ff54bfc5dbb7f8db590894b3e5b9d
J.Zawada <jzawada@pc10.home>
2017-05-06 02:55:55 +0200

    I added button for remove element

--- src/react/elementsList.jsx
+++ src/react/elementsList.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class ElementsList extends React.Component {
-//    constructor(props) {
-//        super(props);
-//        this.state = {
-//            key: this.props.index
-//        }
-//    }
+
     
     render() {
         return (
             <tr>
                 <td>{this.props.value.name}</td>
                 <td>{this.props.value.email}</td>
+                <td style={{cursor: 'pointer'}}>X</td>
             </tr>
         )
     }
